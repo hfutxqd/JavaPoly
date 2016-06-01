e9ca0138be7a5fc5ea4f6ed9dc58d2f3f51d8554
hrjet9 <hrjet9@gmail.com>
2016-06-01 18:19:02 +0530

    Add more eval tests
    (actually renabling them; they were commented out earlier)

--- test/units/eval.js
+++ test/units/eval.js
@@ -1,6 +1,22 @@
 function testEval() {
   describe('Eval', function() {
 
+    it('integer result', function() {
+      return JavaPoly.type('com.javapoly.Eval').then(function(Eval) {
+        Eval.eval("40 + 2").then(function(result) {
+          expect(result).toEqual(42);
+        });
+      });
+    });
+
+    it('array result', function() {
+      return JavaPoly.type('com.javapoly.Eval').then(function(Eval) {
+        Eval.eval("'a,b,c'.split(',')").then(function(result) {
+          expect(result).toEqual(["a", "b", "c"]);
+        });
+      });
+    });
+
     it('should pass all tests from EvalTest.java', function() {
       return JavaPoly.type('EvalTest').then(function(EvalTest) {
         return EvalTest.test().then(function(result) {
@@ -9,40 +25,14 @@ function testEval() {
       });
     });
 
-  });
-}
-
-/* Disabling for now. We can re-enable after using the newer version of eval.
-
-describe('Utilities', function() {
-
-  describe('eval', function() {
-    it('integer arithmetic', function() {
-      return JavaPoly.type('com.javapoly.runtime.Utilities').then(function(Utilities) {
-        Utilities.eval("40 + 2").then(function(result) {
-          expect(result).toEqual(42);
-        });
-      });
-    });
-
-    it('string split', function() {
-      return JavaPoly.type('com.javapoly.runtime.Utilities').then(function(Utilities) {
-        Utilities.eval("'a,b,c'.split(',')").then(function(result) {
-          expect(result).toEqual(["a", "b", "c"]);
-        });
-      });
-    });
-
     it('function definition', function() {
-      return JavaPoly.type('com.javapoly.runtime.Utilities').then(function(Utilities) {
-        Utilities.eval("(function(x){return x*x})").then(function(result) {
+      return JavaPoly.type('com.javapoly.Eval').then(function(Eval) {
+        return Eval.eval("(function(x){return x*x})").then(function(result) {
           expect(result(7)).toEqual(49);
         });
       });
     });
 
   });
-
-});
-*/
+}
 
